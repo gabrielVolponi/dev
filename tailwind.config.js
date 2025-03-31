9ae651be8f52588b7f321ee7f79b15a48cfff853
DESKTOP-AIKJGQ0\gabriel <gabriel.volponi@vidibr.com>
2025-03-31 15:13:49 -0300

    update CSS build script to output to public directory and minify, add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.js"], // Inclua os arquivos HTML e JS onde as classes do Tailwind são usadas
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
